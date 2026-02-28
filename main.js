46db1b55ef9bc9262f2778f7aab6af3fd8a86097
AbhishekAhadi124 <ahadiabhishek@gmail.com>
2026-03-01 00:00:38 +0530

    Initial portfolio upload

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
 menuIcon.onclick = () => {
-    menuIcon.classList.toggle('fa-play');
+    menuIcon.classList.toggle('fa-times');
     navbar.classList.toggle('active');
 }
 
@@ -17,12 +17,19 @@ window.onscroll = () => {
         let  height = sec.offsetHeight;
         let id = sec.getAttribute('id');
 
-          });
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(link => {
+                link.classList.remove('active');
+                document.querySelector(`header nav a[href*=${id}]`).classList.add('active');
+            });
+        }
+
+    });
 
        let header = document.querySelector('header');
     header.classList.toggle('sticky', window.scrollY > 100);
 
-    menuIcon.classList.remove('fa-play');
+    menuIcon.classList.remove('fa-times');
     navbar.classList.remove('active');
 };
 
@@ -33,16 +40,64 @@ ScrollReveal({
     delay: 200,
 });
 
-ScrollReveal().reveal('.home-content, heading', { origin: 'top' });
-ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'button' });
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
 ScrollReveal().reveal('.home-contact h1, .about-img', { origin: 'left' });
-ScrollReveal().reveal('.home-contact P, .about-contact', { origin: 'right' });
+ScrollReveal().reveal('.home-contact p, .about-contact', { origin: 'right' });
 
 const typed = new Typed('.multiple-text', {
-    strings: ['Frontend Developer', 'Web Designer','Data Analyst'],
+    strings: ['Flutter Developer','Frontend Developer','Data Analyst'],
     typeSpeed: 70,
     backSpeed: 70,
     backDelay: 1000,
     loop: true,
 });     
 
+let debounce = (func, wait = 20) => {
+    let timeout;
+    return function () {
+        clearTimeout(timeout);
+        timeout = setTimeout(() => func.apply(this, arguments), wait);
+    };
+};
+window.onscroll = debounce(() => {
+    
+});
+
+    /* ================= PREMIUM PRELOADER ================= */
+
+window.addEventListener("load", () => {
+    setTimeout(() => {
+        const preloader = document.getElementById("preloader");
+        preloader.classList.add("hide");
+    }, 2200); // match progress animation timing
+});
+
+
+
+
+
+
+
+/*window.addEventListener('load', () => {
+    const loader = document.getElementById('loader');
+
+    setTimeout(() => {
+        loader.classList.add('hide');
+    }, 800); 
+});
+*/
+
+
+/*themeIcon.addEventListener("click", () => {
+
+    document.body.classList.toggle("dark-mode");
+
+    if (document.body.classList.contains("dark-mode")) {
+        themeIcon.classList.replace("fa-moon", "fa-sun");
+        localStorage.setItem("theme", "dark");
+    } else {
+        themeIcon.classList.replace("fa-sun", "fa-moon");
+        localStorage.setItem("theme", "light");
+    }
+});*/
